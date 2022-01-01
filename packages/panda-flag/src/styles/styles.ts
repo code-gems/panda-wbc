74d105d5b994b6e1fcaebe980fa2a5ba714f3ffd
code-gems <code-gems@users.noreply.github.com>
2022-01-01 18:30:50 +0800

    initial commit

---
+++ packages/panda-flag/src/styles/styles.ts
@@ -0,0 +1,9 @@
+import { css } from "lit";
+
+export const styles = css`
+    :host {
+        display: inline;
+        width: var(--panda-flag-width);
+        height: var(--panda-flag-height);
+    }
+`;
