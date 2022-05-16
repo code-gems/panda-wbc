3b1ed75cd8fb2f28126954789d375ce2d675d6cb
code-gems <code-gems@users.noreply.github.com>
2022-05-16 12:01:08 +0800

    added new icons

--- packages/panda-icon/src/panda-icon.ts
+++ packages/panda-icon/src/panda-icon.ts
@@ -9,26 +9,33 @@ import { LitElement, html, TemplateResult } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 // icons
-import { folderIcon } from "./resources/folder";
 import { calendarIcon } from "./resources/calendar";
-import { formatBoldIcon } from "./resources/format-bold";
-import { formatUnderlineIcon } from "./resources/format-underline";
-import { formatItalicIcon } from "./resources/format-italic";
-import { formatStrikethroughIcon } from "./resources/format-strikethrough";
-import { formatAlignLeftIcon } from "./resources/format-align-left";
+import { codeIcon } from "./resources/code";
+import { copyIcon } from "./resources/copy";
+import { cutIcon } from "./resources/cut";
+import { editIcon } from "./resources/edit";
+import { expandLessIcon } from "./resources/expand-less";
+import { expandMoreIcon } from "./resources/expand-more";
+import { folderIcon } from "./resources/folder";
 import { formatAlignCenterIcon } from "./resources/format-align-center";
-import { formatAlignRightIcon } from "./resources/format-align-right";
 import { formatAlignJustifyIcon } from "./resources/format-align-justify";
+import { formatAlignLeftIcon } from "./resources/format-align-left";
+import { formatAlignRightIcon } from "./resources/format-align-right";
 import { formatBlockquoteIcon } from "./resources/format-quote";
+import { formatBoldIcon } from "./resources/format-bold";
+import { formatClearIcon } from "./resources/format-clear";
 import { formatIndentDecreaseIcon } from "./resources/format-indent-decrease";
 import { formatIndentIncreaseIcon } from "./resources/format-indent-increase";
-import { formatListNumberedIcon } from "./resources/format-list-numbered";
+import { formatItalicIcon } from "./resources/format-italic";
 import { formatListBulletedIcon } from "./resources/format-list-bulleted";
-import { undoIcon } from "./resources/undo";
+import { formatListNumberedIcon } from "./resources/format-list-numbered";
+import { formatStrikethroughIcon } from "./resources/format-strikethrough";
+import { formatUnderlineIcon } from "./resources/format-underline";
+import { heartIcon } from "./resources/heart";
+import { heartOutlineIcon } from "./resources/heart-outline";
+import { pasteIcon } from "./resources/paste";
 import { redoIcon } from "./resources/redo";
-import { codeIcon } from "./resources/code";
-import { expandMoreIcon } from "./resources/expand-more";
-import { expandLessIcon } from "./resources/expand-less";
+import { undoIcon } from "./resources/undo";
 
 @customElement("panda-icon")
 export class PandaIcon extends LitElement {
@@ -54,24 +61,31 @@ export class PandaIcon extends LitElement {
 			folder: folderIcon,
 
 			// text editor icons
-			["format-bold"]: formatBoldIcon,
-			["format-italic"]: formatItalicIcon,
-			["format-underline"]: formatUnderlineIcon,
-			["format-strikethrough"]: formatStrikethroughIcon,
-			["format-align-left"]: formatAlignLeftIcon,
-			["format-align-center"]: formatAlignCenterIcon,
-			["format-align-right"]: formatAlignRightIcon,
-			["format-align-justify"]: formatAlignJustifyIcon,
-			["format-blockquote"]: formatBlockquoteIcon,
-			["format-indent-increase"]: formatIndentIncreaseIcon,
-			["format-indent-decrease"]: formatIndentDecreaseIcon,
-			["format-list-numbered"]: formatListNumberedIcon,
-			["format-list-bulleted"]: formatListBulletedIcon,
-			undo: undoIcon,
-			redo: redoIcon,
+			"expand-less": expandLessIcon,
+			"expand-more": expandMoreIcon,
+			"format-align-center": formatAlignCenterIcon,
+			"format-align-justify": formatAlignJustifyIcon,
+			"format-align-left": formatAlignLeftIcon,
+			"format-align-right": formatAlignRightIcon,
+			"format-blockquote": formatBlockquoteIcon,
+			"format-bold": formatBoldIcon,
+			"format-indent-decrease": formatIndentDecreaseIcon,
+			"format-indent-increase": formatIndentIncreaseIcon,
+			"format-italic": formatItalicIcon,
+			"format-list-bulleted": formatListBulletedIcon,
+			"format-list-numbered": formatListNumberedIcon,
+			"format-strikethrough": formatStrikethroughIcon,
+			"format-underline": formatUnderlineIcon,
+			"format-clear": formatClearIcon,
 			code: codeIcon,
-			["expand-more"]: expandMoreIcon,
-			["expand-less"]: expandLessIcon,
+			edit: editIcon,
+			redo: redoIcon,
+			undo: undoIcon,
+			cut: cutIcon,
+			copy: copyIcon,
+			paste: pasteIcon,
+			heart: heartIcon,
+			"heart-outline": heartOutlineIcon,
 		};
 	}
 
--- packages/panda-icon/src/styles/styles.ts
+++ packages/panda-icon/src/styles/styles.ts
@@ -1,19 +1,19 @@
 import { css } from "lit";
 
 export const styles = css`
-    :host {
-        display: inline-block;
-        width: var(--panda-icon-size-m, 24px);
-        height: var(--panda-icon-size-m, 24px);
-    }
+	:host {
+		display: inline-block;
+		width: var(--panda-icon-size-m, 24px);
+		height: var(--panda-icon-size-m, 24px);
+	}
 
 	.icon {
 		display: block;
-        width: 100%;
-        height: 100%;
+		width: 100%;
+		height: 100%;
 	}
 
-    svg {
-        fill: var(--panda-icon-fill-color, hsl(0deg 0% 29%));
-    }
+	svg {
+		fill: var(--panda-icon-fill-color, hsl(0deg 0% 29%));
+	}
 `;
